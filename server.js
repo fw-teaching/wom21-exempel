f15b5bbfa6a6f44d8a6a23a3a2628adb3ca88b69
Fredrik Welander <welandfr@arcada.fi>
2021-09-13 16:09:01 +0300

    On branch lektion-2-node-express

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
+require('dotenv').config() // läs in .env-filen
 const express = require('express')
 const app = express()
 const PORT = 3000
 
+console.log(process.env.TEST) // Skriv ut variabel från .env 
+
 console.log("hello node!")
 
-app.use('/', (req, res) => {
+// Vår API ska ta emot requests i JSON-format
+app.use(express.json())
+
+app.get('/', (req, res) => {
     //res.send("Hello Node!")
     res.json({ message: "Hello Node!"})
 })
 
-app.listen(PORT)
+// Vi importerar vår notes route-modul
+const notesRouter = require('./routes/notes')
+app.use('/notes', notesRouter)
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
